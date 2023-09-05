6df5fa299e4a09eeafdeb9ed71371514f5c26f69
unknown <andyhung772@gmail.com>
2023-09-04 22:58:57 -0700

    feat: allow automatic testing

--- taskbox/.storybook/main.js
+++ taskbox/.storybook/main.js
@@ -1,19 +1,23 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
+/**
+ * @format
+ * @type { import('@storybook/react-vite').StorybookConfig }
+ */
+
 const config = {
-  stories: ["../src/components/**/*.stories.@(js|jsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	stories: ['../src/components/**/*.stories.@(js|jsx)'],
+	staticDirs: ['../public'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+	],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
+	docs: {
+		autodocs: 'tag',
+	},
 };
 export default config;
